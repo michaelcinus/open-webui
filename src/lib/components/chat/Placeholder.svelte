25a7d9022b3ac0401a01ea57d2030187f6536a48
michaelcinus <m.cinus@jdk.it>
2026-02-27 17:03:20 +0100

    new style counter

--- src/lib/components/chat/Placeholder.svelte
+++ src/lib/components/chat/Placeholder.svelte
@@ -137,7 +137,7 @@
 						class=" text-3xl @sm:text-3xl line-clamp-1 flex items-center"
 						in:fade={{ duration: 100 }}
 					>
-						{$i18n.t('Hello, {{name}}', { name: $user?.name })}
+						{$i18n.t('Hello, {{name}}', { name: $user?.name })}!
 					</div>
 				</div>
 
